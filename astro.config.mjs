14763f692a8499836eea9b000ed9881597b8f744
Diego GP <diego.gp227@gmail.com>
2026-04-18 19:46:07 -0500

    [ refactor ] migration to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     host: true,
   },
   vite: {
+    plugins: [tailwindcss()],
     server: {
       allowedHosts: true,
     },
